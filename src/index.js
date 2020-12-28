e6093aaf39b80d86563c68a378c14bb4d3ac27a0
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-12-28 10:58:09 +0100

    fixed node sass setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import "./index.scss";
+
 const App = () => {
-  return <h1>Hello world</h1>
+  return <h1>Hello world</h1>;
 };
 
 ReactDom.render(<App />, document.getElementById("root"));
